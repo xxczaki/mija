467c06022549aa9b75b24b623d18e59f6e35d8a7
Antoni Kepinski <keppi@o2.pl>
2018-07-18 22:30:09 +0200

    lint

--- example.js
+++ example.js
@@ -1,23 +1,23 @@
-const mija = require('.')
+const mija = require('.');
 
-mija.info("Please contact us");
+mija.info('Please contact us');
 
-mija.warn("Module is deprecated");
+mija.warn('Module is deprecated');
 
-mija.error("Module is not installed");
+mija.error('Module is not installed');
 
-mija.success("File downloaded");
+mija.success('File downloaded');
 
-mija.magic("Unicorn mode on");
+mija.magic('Unicorn mode on');
 
-mija.inprogress("Test running...");
+mija.inprogress('Test running...');
 
-mija.stop("Not allowed");
+mija.stop('Not allowed');
 
-mija.repeat("Repeat mode on");
+mija.repeat('Repeat mode on');
 
-mija.experimental("Experimental feature");
+mija.experimental('Experimental feature');
 
-mija.danger("Danger zone");
+mija.danger('Danger zone');
 
-mija.custom("😀", "Custom text");
+mija.custom('😀', 'Custom text');
