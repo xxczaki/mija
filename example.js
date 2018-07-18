e0c455c4e86c0589e5265ebcc85bcdec7274e7eb
Antoni Kepinski <keppi@o2.pl>
2018-07-18 22:22:03 +0200

    init

---
+++ example.js
@@ -0,0 +1,23 @@
+const mija = require('.')
+
+mija.info("Please contact us");
+
+mija.warn("Module is deprecated");
+
+mija.error("Module is not installed");
+
+mija.success("File downloaded");
+
+mija.magic("Unicorn mode on");
+
+mija.inprogress("Test running...");
+
+mija.stop("Not allowed");
+
+mija.repeat("Repeat mode on");
+
+mija.experimental("Experimental feature");
+
+mija.danger("Danger zone");
+
+mija.custom("😀", "Custom text");
